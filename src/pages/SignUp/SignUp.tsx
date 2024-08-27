1801bf3f8e775ce5e5c706f10cd4fa0de84af490
João Lee <joaumlimaum@gmail.com>
2024-08-27 15:29:45 -0300

    Create Signup page

--- src/pages/SignUp/SignUp.tsx
+++ src/pages/SignUp/SignUp.tsx
@@ -1,6 +1,69 @@
+import { Lock, Mail, UserRound } from "lucide-react";
+import { FormEvent, useState } from "react";
+
 export default function SignUp() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [displayName, setDisplayName] = useState<string>("");
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(e);
+  }
+
   return (
-    <div>SignUp</div>
+    <section className="form-section mt-4">
+      <p className="text-3xl text-center">Login</p>
+      <form 
+      onSubmit={handleSubmit}
+      className="p-4 text-lg flex flex-col gap-4">
+        <label htmlFor="email">
+          <p>Email</p>
+          <div className="flex items-center gap-1 bg-white p-2 rounded-lg">
+            <Mail />
+            <input
+            onChange={(e) => setEmail(e.target.value)} 
+            value={email} 
+            type="email" 
+            id="email" 
+            className="outline-none w-full"
+            />  
+          </div>
+        </label>
+        <label htmlFor="password">
+          <p>Password</p>
+          <div className="flex items-center gap-1 bg-white p-2 rounded-lg">
+            <Lock />
+            <input 
+            onChange={(e) => setPassword(e.target.value)}
+            value={password} 
+            type="password" 
+            id="password" 
+            className="outline-none w-full"
+            />  
+          </div>
+        </label>
+        <label htmlFor="name">
+          <p>Name</p>
+          <div className="flex items-center gap-1 bg-white p-2 rounded-lg">
+            <UserRound />
+            <input 
+            onChange={(e) => setDisplayName(e.target.value)}
+            value={displayName} 
+            type="text" 
+            id="name" 
+            className="outline-none w-full"
+            />  
+          </div>
+        </label>
+        <button 
+        type="submit"
+        className="border border-bluesy p-2 rounded-lg hover:bg-bluesy hover:text-off-white transition duration-200 w-1/3"
+        >
+          Login
+        </button>
+      </form>
+    </section>
   )
 }
 
