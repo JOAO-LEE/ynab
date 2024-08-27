452aa023a4194efd6a0a961f6484f72f20a26b9a
João Lee <joaumlimaum@gmail.com>
2024-08-27 12:16:11 -0300

    Form styles correction - add control to form inputs

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,22 +1,54 @@
 import { Lock, Mail } from "lucide-react";
+import { FormEvent, useState } from "react";
+import "./Login.css";
 
 export default function Login() {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(e);
+  }
+
   return (
-    <section className="bg-off-white p-8 mx-auto w-full md:w-1/2 rounded shadow-md flex flex-col justify-center">
+    <section className="form-section mt-4">
       <p className="text-3xl text-center">Login</p>
       <form 
-      action="" 
-      className="p-8 text-lg">
-      <label htmlFor="email">Email</label>
-        <div className="flex gap-1 bg-white p-2 rounded-lg">
+      onSubmit={handleSubmit}
+      className="p-4 text-lg flex flex-col gap-4">
+      <label htmlFor="email">
+        <p>Email</p>
+        <div className="flex items-center gap-1 bg-white p-2 rounded-lg">
           <Mail />
-          <input type="email" id="email" className="outline-none w-full"/>  
+          <input
+          onChange={(e) => setEmail(e.target.value)} 
+          value={email} 
+          type="email" 
+          id="email" 
+          className="outline-none w-full"
+          />  
         </div>
-        <label htmlFor="password">Password</label>
-        <div className="flex gap-1 bg-white p-2 rounded-lg ">
+      </label>
+      <label htmlFor="password">
+        <p>Password</p>
+        <div className="flex items-center gap-1 bg-white p-2 rounded-lg">
           <Lock />
-          <input type="password" id="password" className="outline-none w-full"/>  
+          <input 
+          onChange={(e) => setPassword(e.target.value)}
+          value={password} 
+          type="password" 
+          id="password" 
+          className="outline-none w-full"
+          />  
         </div>
+      </label>
+        <button 
+        type="submit"
+        className="border border-bluesy p-2 rounded-lg hover:bg-bluesy hover:text-off-white transition duration-200 w-1/3"
+        >
+          Login
+        </button>
       </form>
     </section>
   )
