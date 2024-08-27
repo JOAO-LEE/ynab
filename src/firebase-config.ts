d5f9ebbf2ebaf02ae03979b3e5ececf9d7091bfe
João Lee <joaumlimaum@gmail.com>
2024-08-27 17:22:17 -0300

    Add Firebase configuration and start creating signup hook

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -1,11 +1,18 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
+import { getApp, getApps, initializeApp, } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from 'firebase/auth';
 
-export const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY ,
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET ,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID ,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const auth = getAuth(app);
+const db = getFirestore();
+
+export { app, auth, db };
---
+++ src/hooks/useSignup.tsx
@@ -0,0 +1,10 @@
+import { useState } from "react";
+import { auth } from "../firebase-config";
+
+function useSignup() {
+  return (
+    <div>useSignup</div>
+  )
+}
+
+export default useSignup
