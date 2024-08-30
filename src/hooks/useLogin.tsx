90225de97bb78ec2d3fe31923ca7e438f351f424
João Lee <joaumlimaum@gmail.com>
2024-08-30 10:12:24 -0300

    Create login hook

---
+++ src/hooks/useLogin.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { useAuthContext } from "./useAuthContext";
+import { auth } from "../firebase-config";
+import { AuthReducerEnum } from "../enum/AuthReducer.enum";
+
+export function useLogin() {
+  const [isCancelled, setIsCancelled] = useState<boolean>(false);
+  const [error, setError] = useState<unknown | null>(null);
+  const [isPending, setIsPending] = useState<boolean>(false);
+  const { dispatch } = useAuthContext();
+
+  const login = async ({ email, password }: { email: string, password: string }) => {
+    setError(null);
+    setIsPending(true);
+
+    try {
+      const response = await signInWithEmailAndPassword(auth, email, password);
+      dispatch({ type: AuthReducerEnum.LOGIN, payload: response.user });
+
+      if (!isCancelled) {
+      setIsPending(false);
+      setError(null);
+      }
+      
+    } catch (error: unknown) {
+      if (!isCancelled) {
+        setError(error);
+        }
+    } finally {
+      setIsPending(false);
+    }
+  }
+
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
+
+  return { error, isPending, login };
+}
+
