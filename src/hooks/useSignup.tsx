9c0acdedcdaede2f139782da915546f180fc1e09
João Lee <joaumlimaum@gmail.com>
2024-08-30 09:48:56 -0300

    Add cleanup function on signup hook

--- src/hooks/useSignup.tsx
+++ src/hooks/useSignup.tsx
@@ -35,9 +35,8 @@ export function useSignup() {
       if (e instanceof Error) {
         if (!isCancelled) {
           setError(e.message);
-          }
+        }
       }
-      
     } finally {
       setIsPending(false);
     }
